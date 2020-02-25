5b181b9f02a12b97bb7a5e4790b48dbf538a5910
Adam Bender <abend07@gmail.com>
2020-02-25 15:50:46 -0700

    In Class Assignment 4 Completed

---
+++ src/business/OrdersBusinessService.java
@@ -0,0 +1,59 @@
+package business;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ejb.Local;
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+import javax.enterprise.inject.Alternative;
+
+import beans.Order;
+
+/**
+ * Session Bean implementation class OrdersBusinessService
+ */
+@Stateless
+@Local(OrderBusinessInterface.class)
+@LocalBean
+@Alternative
+public class OrdersBusinessService implements OrderBusinessInterface {
+
+	List<Order> orders = new ArrayList<Order>();
+
+	/**
+	 * Default constructor.
+	 */
+	public OrdersBusinessService() {
+		orders.add(new Order("00000001", "P1", (float) 1.00, 1));
+		orders.add(new Order("00000002", "P2", (float) 3.00, 2));
+		orders.add(new Order("00000003", "P3", (float) 1.50, 4));
+		orders.add(new Order("00000004", "P4", (float) 2.50, 4));
+		orders.add(new Order("00000005", "P5", (float) 3.50, 4));
+		orders.add(new Order("00000006", "P6", (float) 4.50, 4));
+		orders.add(new Order("00000007", "P7", (float) 5.50, 4));
+		orders.add(new Order("00000008", "P8", (float) 6.50, 4));
+		orders.add(new Order("00000009", "P9", (float) 7.50, 4));
+		orders.add(new Order("00000010", "P10", (float) 8.50, 4));
+	}
+
+	/**
+	 * @see OrderBusinessInterface#test()
+	 */
+	public void test() {
+		System.out.println("Hello from the OrdersBusinessService");
+	}
+
+	@Override
+	public List<Order> getOrders() {
+		// TODO Auto-generated method stub
+		return orders;
+	}
+
+	@Override
+	public void setOrders(List<Order> orders) {
+		// TODO Auto-generated method stub
+		this.orders = orders;
+	}
+
+}
--- src/controllers/FormController.java
+++ src/controllers/FormController.java
@@ -1,19 +1,40 @@
 package controllers;
 
+import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
+import javax.inject.Inject;
 
 import beans.User;
+import business.MyTimerService;
+import business.OrderBusinessInterface;
 
 
 @ManagedBean
 @ViewScoped
 public class FormController {
 	
-	public String onSubmit(User user) {
+	@Inject
+	OrderBusinessInterface service;
+	
+	@EJB
+	MyTimerService timer;
+	
+	public String onSubmit(User user) 
+	{
+		service.test();
+		
+		//set timer to 10s
+		timer.setTimer(10000);
+		
 		FacesContext.getCurrentInstance().getExternalContext().getRequestMap().put("user", user);
 		return "TestResponse.xhtml";
+	}
+	
+	public OrderBusinessInterface getService()
+	{
+		return service;
 	}
 
 }
